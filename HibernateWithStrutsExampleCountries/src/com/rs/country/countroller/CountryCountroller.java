fdcb04eceb2103f420e5745db371649e39d4922a
ramesh <rameshpondugala@gmail.com>
2018-05-08 10:36:15 +0530

    test

--- HibernateWithStrutsExampleCountries/src/com/rs/country/countroller/CountryCountroller.java
+++ HibernateWithStrutsExampleCountries/src/com/rs/country/countroller/CountryCountroller.java
@@ -11,37 +11,29 @@ import org.apache.struts.action.ActionMapping;
 import com.rs.country.bean.Countries;
 import com.rs.country.dao.CountryDao;
 import com.rs.country.dao.CountryImpl;
-import com.rs.country.form.CountryForm;
-import com.rs.country.service.CountryService;
-import com.rs.country.service.CountryServiceImpl;
-
 
 public class CountryCountroller extends Action {
-	
-	
-	//final static Logger logger = Logger.getLogger(FERServiceImpl.class);
+
+	// final static Logger logger = Logger.getLogger(FERServiceImpl.class);
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		CountryDao cd=new CountryImpl();
-	//	CountryService cs=new CountryServiceImpl();
-	//CountryForm cf=(CountryForm)form;
-			Countries cf = new Countries();	
+		CountryDao cd = new CountryImpl();
+		Countries cf = new Countries();
 
 		cf.setCountryCode(request.getParameter("countryCode"));
 		cf.setCountries(request.getParameter("Countries"));
 		cf.setStates(request.getParameter("States"));
 		cf.setDistricts(request.getParameter("Districts"));
-		
+
 		boolean registration = cd.SaveCountries(cf);
 		if (registration) {
-			return mapping.findForward("success"); 
-			
-		}else{
+			return mapping.findForward("success");
 
-		return mapping.findForward("falied");
-	}
+		} else {
+
+			return mapping.findForward("falied");
+		}
 	}
 
-	
 }
